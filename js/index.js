d437a25c27384e924912ea947ec4c2d03b9d1101
rimiruma <rimiruma12@gmail.com>
2024-09-23 15:29:22 +0600

    added navbar done

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-donate now').addEventListener('click', function(event){
+    event.preventDefault();
+    // console.log('login button clicked');
+    const AmountDonation = document.getElementById('donation-amount').value;
+    console.log(AmountDonation)
+    
+})
